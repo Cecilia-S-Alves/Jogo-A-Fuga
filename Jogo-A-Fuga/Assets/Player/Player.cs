32e5b7dcbe34651df06f2ac5abc4eeb0b9970dc5
Ale <Joaoalejandrobrenisborges@gmail.com>
2024-11-19 17:21:12 -0300

    bb

--- Jogo-A-Fuga/Assets/Player/Player.cs
+++ Jogo-A-Fuga/Assets/Player/Player.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float speed = 10;
+    public float jumpForce = 5;
+    private Rigidbody2D rig;
+    public Animator anim;
+    public bool isground;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator> ();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+         Move();
+         Jump();
+    }
+
+    void Move()
+    {
+        float tecla = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(tecla * speed, rig.velocity.y);
+
+        //if (tecla > 0 && isground == true)
+        {
+           // transform.eulerAngles = new Vector3(0, 0);
+        //    anim.SetInteger("transitions", 1);
+        }
+
+        //if (tecla < 0 && isground == true)
+        {
+            // transform.eulerAngles = new Vector3(0, 180);
+            //   anim.SetInteger("transitions", 1);
+        }
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isground)
+        { 
+            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            //anim.SetInteger("transitions", 2);
+            isground = false;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D colisao)
+    {
+        if (colisao.gameObject.tag == "ground")
+        {
+            isground = true;
+            Debug.Log(isground);
+        }
     }
 }
